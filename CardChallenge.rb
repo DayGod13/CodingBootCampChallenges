17a4ecb3ca81fe2a31f4ee46ec8e7d21f717fe09
Tobias Pasquale <tobiaspasquale@gmail.com>
2019-06-06 05:00:07 +0000

    Deck of cards coding challenge

--- CardChallenge.rb
+++ CardChallenge.rb
@@ -10,9 +10,29 @@ class Card
       puts "#{self.rank} of #{self.suit}"
     end
   
-    def self.random_card
-      Card.new(rand(10), :spades)
-    end
   end
 
-  puts 15
+  class Deck
+    def initialize
+        @cards = []
+        @ranks = [*(2..10), 'J', 'Q', 'K', 'A']
+        @suits = [:spades, :diamonds, :clubs, :hearts]
+        
+        @ranks.each do |rank|
+            @suits.each do |suit|
+                @cards << Card.new(rank, suit)
+            end
+        end
+
+        @cards.shuffle!
+    end
+
+    def deal (number)
+        number.times {@cards.shift.output_card}
+    
+    end
+
+  end
+
+  deck = Deck.new
+  deck.deal(10)
