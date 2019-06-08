ab065797981cf8126e0efdd9e49c8116b6d9a0da
Tobias Pasquale <tobiaspasquale@gmail.com>
2019-06-08 15:12:30 +0000

    Added code for Manhattan distance

---
+++ ImageBlur3.rb
@@ -0,0 +1,54 @@
+class Image
+    attr_accessor :image
+
+    def initialize array
+        @image = array
+    end
+
+    def output_image
+        @image.each do |row|
+          puts row.join
+        end
+    end
+
+    def blur(distance=1)
+        distance.times do
+          blur_cords
+        end
+    end
+
+    private 
+
+    def blur_cords
+        blur_cords = []
+        @image.each_with_index do |row, i|
+            row.each_with_index do |x, row_i|
+                blur_cords << [i, row_i] if x == 1
+            end
+        end
+
+        blur_cords.each do |cord|
+            @image[cord[0]][cord[1] + 1] = 1 if cord[1] + 1 <= @image[cord[0]].length - 1
+            @image[cord[0]][cord[1] - 1] = 1 if cord[1] - 1 >= 0
+            @image[cord[0] + 1][cord[1]] = 1 if cord[0] + 1 <= @image.length - 1
+            @image[cord[0] - 1][cord[1]] = 1 if cord[0] - 1 >= 0
+        end
+    end
+
+end
+
+image = Image.new([
+    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+  ])
+
+image.blur(3)
+image.output_image
